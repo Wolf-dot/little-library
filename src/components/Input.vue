8216f57853f90d584808481672efb60a17f547b2
[Wolf-dot] <[GoUpv13@gmail.com]>
2021-05-30 23:02:37 +0200

    update input

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,21 +1,30 @@
 <template>
-  <input type="text" name="text" id="text" v-model="text">
+  <input type="text" name="text" id="text" v-model="text" /> <br />
+  <p v-if="error.is" class="error">{{ error.message }}</p>
   <button @click.prevent="submit">OK</button>
 </template>
 
 <script>
 export default {
-    name: "Input",
-    emits: ['input'],
-    data(){
-        return{
-            text: ""
-        }
+  name: "Input",
+  emits: ["input"],
+  props: {
+    error: Object,
+  },
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    submit() {
+      if (this.text === "") {
+        this.$props.error.is = true;
+      } else {
+        this.$props.error.is = false;
+        this.$emit("input", this.text);
+      }
     },
-    methods: {
-        submit(){
-            this.$emit('input', this.text)
-        }
-    }
-}
+  },
+};
 </script>
