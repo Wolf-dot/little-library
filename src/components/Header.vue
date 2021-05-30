320b88d122d392528cfab33d0020212a6ae186ea
[Wolf-dot] <[GoUpv13@gmail.com]>
2021-05-30 23:00:46 +0200

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="header">
-        <h3>Hello, {{ name }}.</h3>
-        <h1>Welcome to your Little Library</h1>        
-    </div>
+  <nav>
+    <h1><a href="">Little Library</a></h1>
+    <input type="checkbox" id="menu-toggle" />
+    <label for="menu-toggle">&#9776;</label>
+    <a href="https://glitch.com/@Wolf-dot">GLITCH</a>
+    <a href="https://github.com/Wolf-dot">GITHUB</a>
+    <a href="tvmaze.com">TV MAZE</a>
+  </nav>
 </template>
