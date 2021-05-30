d19c9cf85782846edbb886044334d8ee1e203b9c
[Wolf-dot] <[GoUpv13@gmail.com]>
2021-05-30 22:59:11 +0200

    update result showing

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,9 +1,23 @@
 <template>
-    <div class="results">
-        <div v-for="result in results.docs" :key="result.cover_i">
-            {{result.title}}
-        </div> 
-    </div>  
+  <article>
+    <div class="result" v-for="result in results" :key="result.show.id">
+      <div v-if="result.show.image">
+        <img :src="result.show.image.medium" alt="show cover" />
+      </div>
+      <div v-else>
+        <div class="rectangle">no image</div>
+      </div>
+      <h3>
+        {{ result.show.name }}
+      </h3>
+      <p>
+        <mark v-if="result.show.rating.average">
+          {{ result.show.rating.average }}
+        </mark>
+      </p>
+      <mark @click="showMore(result)">More info</mark>
+    </div>
+  </article>
 </template>
 
 <script>
