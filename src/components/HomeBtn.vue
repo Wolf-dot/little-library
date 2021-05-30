a292ddf95ed6561df88406151abd2106f2ed2583
[Wolf-dot] <[GoUpv13@gmail.com]>
2021-05-30 23:09:24 +0200

    add a home button

---
+++ src/components/HomeBtn.vue
@@ -0,0 +1,14 @@
+<template>
+  <button type="button" @click="home">home</button>
+</template>
+
+<script>
+export default {
+    name: "HomeBtn",
+    methods: {
+        home(){
+            this.$emit('home')
+        }
+    }
+}
+</script>
