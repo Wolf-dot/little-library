1f629a2f32897b4e13deb66aa45e36100c868799
[Wolf-dot] <[GoUpv13@gmail.com]>
2021-05-30 22:57:32 +0200

    use bahunya for style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,17 +4,3 @@
         <h1>Welcome to your Little Library</h1>        
     </div>
 </template>
-
-<script>
-export default {
-    name: 'Header',
-    props: {
-        name: String
-    }
-}
-</script>
-
-<style>
-
-
-</style>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -19,23 +19,3 @@ export default {
     }
 }
 </script>
-
-<style>
-    input[type=text] {
-        text-align: center;
-        font-size: 1.3em;
-        width: 100%;
-        padding: 12px 20px;
-        box-sizing: border-box;
-        border: none;
-        border-bottom: 2px solid olive;
-    }
-    button {
-        border: none;
-        color: white;
-        margin: 10px;
-        padding: 1em;
-        background-color: olive;
-        border-radius: 50px;
-    }
-</style>
